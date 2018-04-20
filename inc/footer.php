c58b5259915eaffd811cd5247699d15fd6576005
CodeWithFavour <37211195+favcode54@users.noreply.github.com>
2018-04-20 09:10:50 -0500

    u[dated branch

---
+++ inc/footer.php
@@ -0,0 +1,141 @@
+
+
+<!--FOOTER SECTION START HERE-->
+
+<footer class="ParalaxImage paraxify" style="background-image:url('assets/images/tour.jpg');">
+    <div class="footerContainer">
+        <div class="container footerPadding">
+            <div class="row">
+
+                <!--footer menu-->
+                <div class="col-md-12 col-xs-12 col-sm-12 footerMb">
+                    <div class="row white">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <h3 class="white RobotoBold footerLogo">
+                                <img class="img-responsive" src="assets/images/logo-white.png" alt="Logo"/>
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias, at blanditiis eius error.
+                            </p>
+                            <ul>
+                                <li>Mobile no: 131-165-6515</li>
+                                <li>Email:- info@yourdomain.com</li>
+                            </ul>
+                        </div>
+                        <div class="col-md-7 col-md-offset-1 col-sm-8">
+                            <div class="col-md-4 col-xs-4  col-sm-4">
+                                <h3 class="white RobotoBold footerTitle footerSmall">Conditions</h3>
+                                <ul>
+                                    <li><a href="javascript:;">Terms and Conditions</a></li>
+                                    <li><a href="javascript:;">Privacy Policy</a></li>
+                                    <li><a href="javascript:;">Rights Reserved</a></li>
+                                    <li><a href="javascript:;">Support</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-4 col-xs-4 col-sm-4">
+                                <h3 class="white RobotoBold footerTitle footerSmall">Navigation</h3>
+                                <ul>
+                                    <li><a href="javascript:;">About Us</a></li>
+                                    <li><a href="javascript:;">How it works</a></li>
+                                    <li><a href="javascript:;">Why choose us</a></li>
+                                    <li><a href="javascript:;">Plans</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-4 col-xs-4  col-sm-4">
+                                <h3 class="white RobotoBold footerTitle footerSmall">Locations</h3>
+                                <ul>
+                                    <li><a href="javascript:;">london</a></li>
+                                    <li><a href="javascript:;">Germany</a></li>
+                                    <li><a href="javascript:;">New York</a></li>
+                                    <li><a href="javascript:;">San Francisco</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--rights reserved line-->
+                <div class="col-md-12 col-xs-12 col-sm-12 rightsReserved">
+                    <div class="row white">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <p>
+                                &copy; Copyright 2017, all rights reserved by <a href="https://themeforest.net/user/design_gurus" target="_blank" class="gradientColor">Design Gurus</a>.
+                            </p>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12 text-right">
+                            <ul class="list-inline SocialIcons">
+                                <li>
+                                    <a href="javascript:;">
+                                        <img src="assets/images/footer/fb.png" alt="Facebook Logo" class="img-responsive footerImages"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">
+                                        <img src="assets/images/footer/li.png" alt="LinkedIn Logo" class="img-responsive footerImages"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">
+                                        <img src="assets/images/footer/gm.png" alt="Gmail Logo" class="img-responsive footerImages"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">
+                                        <img src="assets/images/footer/pi.png" alt="Pinterest Logo" class="img-responsive footerImages"/>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!--FOOTER SECTION END HERE-->
+
+<!-- GO TO TOP FUNCYION START HERE-->
+
+<button onclick="goTopFunction()" id="TopBtn" class="gradientbgTransparent btn btn-gradient"><i class="visible-xs fa fa-arrow-up"></i><span class="hidden-xs">Back To Top</span></button>
+
+<!-- GO TO TOP FUNCYION END HERE-->
+
+
+<!-- All Javascripts -->
+
+<!-- Jquery -->
+<script type="text/javascript" src="assets/js/jquery-3.2.1.js"></script>
+
+<!-- Bootstrap -->
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+<!-- Nice Scroll -->
+<script type="text/javascript" src="assets/plugins/niceScroll/niceScroll.min.js"></script>
+
+<!-- Owl Carousel -->
+<script type="text/javascript" src="assets/plugins/owl/owl.carousel.js"></script>
+
+<!-- Animation Js -->
+<script type="text/javascript" src="assets/plugins/aos-master/aos.js"></script>
+
+<!--Paraxify js here-->
+<script type="text/javascript" src="assets/plugins/paraxify/paraxify.js"></script>
+
+
+<!-- Number Counter -->
+<script type="text/javascript" src="assets/plugins/numScroll/numscroller-1.0.js"></script>
+
+
+<!-- common js file -->
+<script type="text/javascript" src="assets/js/main.js"></script>
+
+<!-- gradient variation home js file -->
+<script type="text/javascript" src="assets/js/gradient.js"></script>
+
+
+</body>
+
+<!-- Mirrored from edutech.designgurus.in/color1.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 Apr 2018 11:18:39 GMT -->
+</html>
---
+++ inc/header.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Education - Single page Multipurpose Reponsisve bootstrap3 template</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+    <link rel="shortcut icon" href="assets/images/favicon.png">
+
+    <!--Bootstrap css here-->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!--Font-Awsome css here-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!--Owl-carousel css here-->
+    <link rel="stylesheet" href="assets/plugins/owl/owl.carousel.css">
+    <link rel="stylesheet" href="assets/plugins/owl/owl.theme.default.css">
+    <link rel="stylesheet" href="assets/plugins/owl/owl.transitions.css">
+
+	<!--Animtion css here-->
+    <link rel="stylesheet" href="assets/plugins/aos-master/aos.css">	
+		
+    <!--common css here-->
+    <link rel="stylesheet" href="assets/css/gradient.css">
+
+    <!-- Gradient Color css-->
+    <link rel="stylesheet" href="assets/css/color1.css">
+
+    <!--Responsive css here-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+    <!--Paraxify css here-->
+    <link rel="stylesheet" href="assets/plugins/paraxify/paraxify.css"/>
+
+</head>
+<body>
+
+
+<!---loader-->
+
+<div class="pageLoader">
+    <div class="loaderArea">
+        <div class="Myloader1">
+            <div class="gradient_circle"></div>
+        </div>
+    </div>
+</div>
+
+<!---loader-->
+
+
+<!-- header -->
+<header class="paraxify">
+    <!--top bar-->
+    <div class="overlay">
+        <!--top bar-->
+        <!--navigation-->
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header logoo">
+                    <button id="tog-btn" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#banner-section">
+                       <img class="img-responsive headerLogo" src="assets/images/logo-white.png" width="110" alt="Logo"/>
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav GradinetNav navbar-nav navbar-right hidden-xs hidden-sm">
+                        <li>
+                            <a href="javascript:;" class="btn borderBTn" style="margin-bottom: 0px;" data-toggle="modal" data-target="#popupRegister">Account</a>
+                        </li>
+                    </ul>
+                    <ul id="navigation" class="nav navbar-nav navbar-right">
+                        <li class="active dropdown">
+                            <a class="dropdown-toggle"  data-val="#banner-section" data-toggle="dropdown" href="#">Home <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="color1.html">Gradient 1</a></li>
+                                <li><a href="color2.html">Gradient 2</a></li>
+                                <li><a href="color3.html">Gradient 3</a></li>
+                            </ul>
+                        </li>
+                       <li><a href="#how-it-works" data-val="#how-it-works">About</a></li>
+                        <li><a href="#popular" data-val="#popular">Programs</a></li>
+                        <li><a href="#services" data-val="#services">Services</a></li>
+                        <li><a href="#faq" data-val="#faq">F.A.Q</a></li>
+                        <li><a href="#blog" data-val="#blog">Blog</a></li>
+                        <li><a href="#contactUs" data-val="#contactUs">Contact us</a></li>
+                        <li class="visible-xs"><a href="#">Register</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+        <!-- NAVIGATION -->
